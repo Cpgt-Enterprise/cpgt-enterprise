b012d42c4da9e9196e240921bdaaae0ae738cecc
DESKTOP-6Q9O45Q\CPGT <brunorodrigues.12@hotmail.com>
2023-09-07 11:56:21 -0300

    Inicio

--- cpgt_os/urls.py
+++ cpgt_os/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path("", include("cpgt_os.urls")),
-    path("admin/", admin.site.urls)
+    path("", views.index, name="index"),
 ]
