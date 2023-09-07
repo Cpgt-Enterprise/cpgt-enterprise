c4dc4d5624a3a38c9e3a4017026e3beb229b5a19
DESKTOP-6Q9O45Q\CPGT <brunorodrigues.12@hotmail.com>
2023-09-07 12:39:12 -0300

    Alteração para render index

--- os_cpgt/urls.py
+++ os_cpgt/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('index/', include('cpgt_os.urls')),
+    path('cpgt-enterprise/', include('cpgt_os.urls')),
     path('admin/', admin.site.urls),
 ]
